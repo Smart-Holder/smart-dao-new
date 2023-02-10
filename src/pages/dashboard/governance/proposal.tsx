078cc9bdc6925dbab33979fc5f171a824b2d535d
MagnumGOYB <sosbs@outlook.com>
2023-02-10 09:48:23 +0800

    governance proposal page

--- src/pages/dashboard/governance/proposal.tsx
+++ src/pages/dashboard/governance/proposal.tsx
@@ -11,13 +11,13 @@ const App = () => {
   return (
     <DashboardLayout>
       <Layout.Content className={styles['dashboard-content']}>
-        <div>
-          <div className={styles.title1}>Governance</div>
-          <div className={styles.title2}>Welcome to SmartDAO</div>
+        <div className={styles['dashboard-content-header']}>
+          <div>
+            <div className={styles.title1}>Basic Settings</div>
+            <div className={styles.title2}>Welcome to SmartDAO</div>
+          </div>
         </div>
-        <div className={styles.box}>
-          <Proposal />
-        </div>
+        <div className={`${styles['dashboard-content-body']}`}></div>
       </Layout.Content>
     </DashboardLayout>
   );
