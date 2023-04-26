9b32612eabd8c4b7a86f6d045fbd71dd80f706f8
gaoning <gaoning@jinmingdeMacBook-Pro.local>
2023-04-26 16:53:05 +0800

    update

--- src/config/define_ext.ts
+++ src/config/define_ext.ts
@@ -1,31 +1,30 @@
-
-import { DAO,AssetOrder,Asset, Member } from './define';
+import { DAO, AssetOrder, Asset, Member, AssetOwner } from './define';
 
 export interface AssetOrderExt extends AssetOrder {
-	asset_id?: number,
-	asset?: Asset;
+  asset?: Asset;
 }
 
 export interface AssetExt extends Asset {
-	dao?: DAO;
+  dao?: DAO;
+  asset_owner?: AssetOwner;
 }
 
 export interface DAOExtend extends DAO {
-	isMember?: boolean;
-	isLike?: boolean;
-	memberObjs?: Member[];
+  isMember?: boolean;
+  isLike?: boolean;
+  memberObjs?: Member[];
 }
 
 export interface DAOSummarys {
-	membersTotal: number;      // members total
-	voteProposalTotal: number; // all proposals total
-	voteProposalPendingTotal: number; // ongoing proposals
-	voteProposalExecutedTotal: number; // resolutions complete executed
-	voteProposalResolveTotal: number; // resolve total
-	voteProposalRejectTotal: number; // reject total
-	assetTotal: number;            // asset total
-	assetAmountTotal: string;      // asset total amount value
-	assetOrderTotal: number;       // Asset order total
-	assetOrderAmountTotal: string; // Asset order total amount value
-	assetLedgerIncomeTotal: string; // Asset Ledger total Income value
+  membersTotal: number; // members total
+  voteProposalTotal: number; // all proposals total
+  voteProposalPendingTotal: number; // ongoing proposals
+  voteProposalExecutedTotal: number; // resolutions complete executed
+  voteProposalResolveTotal: number; // resolve total
+  voteProposalRejectTotal: number; // reject total
+  assetTotal: number; // asset total
+  assetAmountTotal: string; // asset total amount value
+  assetOrderTotal: number; // Asset order total
+  assetOrderAmountTotal: string; // Asset order total amount value
+  assetLedgerIncomeTotal: string; // Asset Ledger total Income value
 }
